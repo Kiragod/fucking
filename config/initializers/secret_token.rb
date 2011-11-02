f8257f9b82c9f634d2f95818045ae6a5ea1be9ff
kiragod <Kiragods@gmail.com>
2011-11-02 15:24:32 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fucking::Application.config.secret_token = '3aae57b9cd1148877ac7f0536e079b0d71904d5e6b7fc245f098a6edc4beb25961fc41784da974673fb8fab1231a68ee5d41fff08ef9b2e63cec7d5f970f69c5'
